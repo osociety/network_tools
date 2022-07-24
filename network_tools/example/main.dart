53f368f45e1ff05d722df5f64ca1dc1d38d5acbb
guyluz11 <guyluz11@gmail.com>
2022-07-24 14:02:07 +0300

    Remove Future<void> from main function in example folder

--- network_tools/example/main.dart
+++ network_tools/example/main.dart
@@ -1,4 +1,4 @@
-Future<void> main() async {
+void main() {
   print("Run host scan : 'dart example/host_scan.dart'");
   print("Run port scan : 'dart example/port_scan.dart'");
   print("Run mdns scan : 'dart example/mdns_scan.dart'");
