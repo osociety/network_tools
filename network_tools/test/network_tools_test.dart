832dcf18a7d9e12f414b36a682fc7a641d0ee173
git-elliot <fs0c19ty@protonmail.com>
2023-03-09 22:17:38 +0530

    Changing port to ssh

--- network_tools/test/network_tools_test.dart
+++ network_tools/test/network_tools_test.dart
@@ -1,4 +1,4 @@
-const port = 53; //ssh (22) is for github actions and dns (53) for local machine
+const port = 22; //ssh (22) is for github actions and dns (53) for local machine
 const timeout = Duration(milliseconds: 3000);
 const lastHostId =
     1024; //keep it 128 for local tests, and 1024 for github actions
