3193edb186fea058b7ccea7f8d7c8bcdfe6fb259
git-elliot <fs0c19ty@protonmail.com>
2023-03-09 19:43:36 +0530

    Trying to reduce timeout

--- network_tools/test/network_tools_test.dart
+++ network_tools/test/network_tools_test.dart
@@ -1,3 +1,3 @@
 const port = 22; //ssh (22) is for github actions and dns (53) for local machine
-const timeout = Duration(milliseconds: 5000);
+const timeout = Duration(milliseconds: 500);
 void main() {}
