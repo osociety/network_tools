f1156547744ca74fd6ed8dd955aaae3c525fb563
git-elliot <fs0c19ty@protonmail.com>
2025-08-15 15:44:03 +0530

    Corrected examples

--- example/lib/main.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  // ignore: avoid_print
-  print("Run host scan : 'dart example/lib/scan/host_scan.dart'");
-  // ignore: avoid_print
-  print("Run port scan : 'dart example/lib/scan/port_scan.dart'");
-  // ignore: avoid_print
-  print("Run mdns scan : 'dart example/lib/scan/mdns_scan.dart'");
-}
---
+++ example/main.dart
@@ -0,0 +1,21 @@
+import 'package:network_tools/network_tools.dart';
+
+void main() async {
+  // Example: Scan for active hosts in the local subnet (e.g., 192.168.1)
+  final subnet = '192.168.1';
+  print('Scanning for active hosts in ' + subnet + '.0/24 ...');
+
+  await for (final host in HostScannerServiceImpl().getAllPingableDevices(
+    subnet,
+  )) {
+    // ignore: avoid_print
+    print('Found host: ' + host.internetAddress.address);
+  }
+  // For more examples, you can run the following commands:
+  // ignore: avoid_print
+  print("Run host scan : 'dart example/lib/scan/host_scan.dart'");
+  // ignore: avoid_print
+  print("Run port scan : 'dart example/lib/scan/port_scan.dart'");
+  // ignore: avoid_print
+  print("Run mdns scan : 'dart example/lib/scan/mdns_scan.dart'");
+}
