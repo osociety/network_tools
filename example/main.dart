805d3f4e50827bdeb45aff84958e96e40ff04621
git-elliot <fs0c19ty@protonmail.com>
2025-08-15 15:48:36 +0530

    fix example

--- example/main.dart
+++ example/main.dart
@@ -4,8 +4,9 @@ void main() async {
   // Example: Scan for active hosts in the local subnet (e.g., 192.168.1)
   final subnet = '192.168.1';
   print('Scanning for active hosts in ' + subnet + '.0/24 ...');
+  await configureNetworkTools('build', enableDebugging: true);
 
-  await for (final host in HostScannerServiceImpl().getAllPingableDevices(
+  await for (final host in HostScannerService.instance.getAllPingableDevices(
     subnet,
   )) {
     // ignore: avoid_print
